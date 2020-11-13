88d1ea2672b6c81fbd3ccbf905567e9ce20fa0ab
okorkov <alex.okorkov@gmail.com>
2020-11-13 10:25:38 -0800

    commit tree e81197b8cd1a0402ad4d0bcec0470e198c80b01a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new
+    end 
+    def roster
+        @roster
+    end
+    def add_student(name, grade)
+        if @roster.key?(grade)
+            @roster[grade] << name
+        elsif @roster.key?(grade) == false
+            @roster[grade] = [name]
+        end
+    end
+    def grade(grade)
+       @roster[grade]
+    end
+    def sort
+        @roster.map do |key, value|
+            @roster[key].sort! { |a, b| a <=> b }
+        end
+        @roster.sort_by {|k, v| k}.to_h
+    end
+end
+
